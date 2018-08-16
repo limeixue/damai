44ac7d92fa740732d6c9fd39bbc0866eb88cbdef
limeixue <limeixue1205.163.com>
2018-08-16 08:02:42 +0800

    Signed-off-by: limeixue <limeixue1205.163.com>

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Home from "@/components/Home"
-import Login from "@/pages/Login"
+import Login from "@/pages/Login/Login"
 
 Vue.use(Router)
 
